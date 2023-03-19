b81183d7685945aa120e623675e854035e8f8a19
jveenland <33731086+jveenland@users.noreply.github.com>
2023-03-20 00:44:04 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-    name="brats",
-    version="0.2",
+    name="ktml",
+    version="0.3",
     description="""Final Project TM10007ML""",
     license="Apache 2.0 License",
     author="Martijn Starmans, Karin van Garderen, Hakim Achterberg",
     author_email="m.starmans@erasmusmc.nl",
+    install_requires=required,
     include_package_data=True,
     packages=[
         "brats",
         "hn",
-        "adni"
+        "adni",
+        "worclipo",
+        "worcliver",
+        "worccrlm",
+        "worcdesmoid",
+        "worcgist"
     ],
 )
