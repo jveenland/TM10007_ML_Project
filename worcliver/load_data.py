537ed7bad78c93030fd6f5cf60af627f7773e09c
DGJ Pasqualini <d.g.j.pasqualini@student.tudelft.nl>
2023-03-25 20:30:07 +0100

    Tried something for the loading of the datasets.

--- worcliver/load_data.py
+++ worcliver/load_data.py
@@ -7,3 +7,17 @@ def load_data():
     data = pd.read_csv(os.path.join(this_directory, 'Liver_radiomicFeatures.csv'), index_col=0)
 
     return data
+
+
+def load_Ft_set():
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    Ft_usable = pd.read_csv(os.path.join(this_directory, 'Ft_set.csv'), index_col=0)
+
+    return Ft_usable
+
+
+def load_D_set():
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    D_usable = pd.read_csv(os.path.join(this_directory, 'D_set.csv'), index_col=0)
+
+    return D_usable
